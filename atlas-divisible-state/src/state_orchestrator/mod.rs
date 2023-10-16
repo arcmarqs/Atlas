1a6100dee17e271118836d63e26b92b91bd8dd1c
arcmarqs <maramadeu@gmail.com>
2023-10-16 15:06:38 +0100

    increase prefix size

--- atlas-divisible-state/src/state_orchestrator/mod.rs
+++ atlas-divisible-state/src/state_orchestrator/mod.rs
@@ -7,7 +7,7 @@ use crate::{
 use atlas_common::collections::HashSet;
 use serde::{Deserialize, Serialize};
 use sled::{Config, Db, Mode, Subscriber, IVec,};
-pub const PREFIX_LEN: usize = 2;
+pub const PREFIX_LEN: usize = 3;
 
 #[derive(Clone, Debug, PartialEq, Eq, Ord, PartialOrd, Serialize, Deserialize,Hash)]
 pub struct Prefix(pub [u8;PREFIX_LEN]);
